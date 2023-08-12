d008fbf55d1e8f161f6bec85843202f30702dae0
Rachala Ovin <resistlyfe@gmail.com>
2023-08-12 12:04:12 +0530

    chore: remove debug from upload script

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -7,9 +7,9 @@ function upload_to_package_registry() {
 	curl --header "JOB-TOKEN: ${CI_JOB_TOKEN}" --upload-file $upload_file ${PACKAGE_REGISTRY_URL}/$upload_target
 }
 
-# upload_to_package_registry "bin/$FINAL_BIN_NAME" "$FINAL_BIN_NAME"
+upload_to_package_registry "bin/$FINAL_BIN_NAME" "$FINAL_BIN_NAME"
 
 for file in $(ls bin/tab_completions/); do
 	file_path="bin/tab_completions/$file"
-	# upload_to_package_registry $file_path "tab_completions/$file"
+	upload_to_package_registry $file_path "tab_completions/$file"
 done
