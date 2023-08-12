75bdeed76efc93e4ece60a7ec353b3fb51d23c6b
Rachala Ovin <resistlyfe@gmail.com>
2023-08-12 11:58:53 +0530

    fix: wrong shell ls ref

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-echo "Running upload with $FINAL_BIN_NAME | $PACKAGE_REGISTRY_URL"
-
 function upload_to_package_registry() {
 	upload_file=$1
 	upload_target=$2
@@ -9,9 +7,9 @@ function upload_to_package_registry() {
 	curl --header "JOB-TOKEN: ${CI_JOB_TOKEN}" --upload-file $upload_file ${PACKAGE_REGISTRY_URL}/$upload_target
 }
 
-upload_to_package_registry "bin/$FINAL_BIN_NAME" "$FINAL_BIN_NAME"
+# upload_to_package_registry "bin/$FINAL_BIN_NAME" "$FINAL_BIN_NAME"
 
-for file in ls bin/tab_completions; do
+for file in $(ls bin/tab_completions/); do
 	file_path="bin/tab_completions/$file"
-	upload_to_package_registry $file_path "tab_completions/$file"
+	# upload_to_package_registry $file_path "tab_completions/$file"
 done
